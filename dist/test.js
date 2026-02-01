c3a3f283519bade6df8c7c55e79f61d7c8157acf
Back.end.forum <back.end.forum.php@gmail.com>
2026-02-01 21:22:03 +0400

    Type Annotations And Any Data Type

--- dist/test.js
+++ dist/test.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let name = "mustafa";
-let number = 456654;
-let isAvailable = true;
 let all;
+all = "name";
+all = true;
+all = 41;
+let myArray = ["html", "css", "js", null, undefined, "test"];
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]?.repeat(10));
+}
 //# sourceMappingURL=test.js.map
